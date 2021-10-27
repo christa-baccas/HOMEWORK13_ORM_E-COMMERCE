de3a1be4a18747690324c0e3afa191ede5d09a82
christa-baccas <christa.baccas@gmail.com>
2021-10-27 17:19:06 -0500

    added route to post a category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -35,6 +35,12 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const createData = await Category.create(req.body);
+    res.status(200).json(createData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.put('/:id', (req, res) => {
