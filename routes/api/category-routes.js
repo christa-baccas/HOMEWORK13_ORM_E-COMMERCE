f10ed307099c1c43258e5888db4fb4b33c3c7209
christa-baccas <christa.baccas@gmail.com>
2021-10-27 17:36:30 -0500

    added delete route to category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -33,7 +33,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
   try {
     const createData = await Category.create(req.body);
@@ -47,8 +47,23 @@ router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deleteData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    if (!deleteData){
+      res.status(404).json({note: 'No category found for that ID!'});
+      return;
+    }else {
+      res.status(200).json(deleteData);
+    } 
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
