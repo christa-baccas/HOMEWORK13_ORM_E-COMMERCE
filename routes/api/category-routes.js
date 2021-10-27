73b0b52a89703c4d6d10114a43963e996c3131da
christa-baccas <christa.baccas@gmail.com>
2021-10-27 18:01:51 -0500

    added to update data in category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,8 +43,23 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    if(!updateCategory){
+      res.status(404).json({note:'No category found for that ID to update'});
+      return;
+    } else {
+      res.status(200).json(updateCategory);
+    }
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.delete('/:id', async (req, res) => {
@@ -56,7 +71,7 @@ router.delete('/:id', async (req, res) => {
       }
     })
     if (!deleteData){
-      res.status(404).json({note: 'No category found for that ID!'});
+      res.status(404).json({note: 'No category found for that ID to delete'});
       return;
     }else {
       res.status(200).json(deleteData);
